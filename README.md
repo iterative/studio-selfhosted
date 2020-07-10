d164abe0bb209d2794222f4a5dca329ddfced3f3
Guro Bokum <jiojiajiu@gmail.com>
2020-07-10 17:51:08 +0000

    Docker-compose MVP
    Relates iterative/viewer#623

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 Bootstrap for running your own DVC Viewer
 
+## Getting Started
+
+1. Get access to docker images via aws credentials  
+    There are 2 options
+    * `eval $(aws ecr get-login --no-include-email)`
+    * install and setup [amazon-ecr-credentials-helper](https://github.com/awslabs/amazon-ecr-credential-helper)
+2. Create [Github OAuth](./docs/02-github-oauth.md)
+
 ## Documentation
 
-* [Supported enviroment variables](./docs/env-variables.md)
+* [Supported enviroment variables](./docs/01-env-variables.md)
--- start.sh
+++ start.sh
@@ -2,14 +2,19 @@
 
 export DVC_VIEWER_BACKEND_IMAGE=260760892802.dkr.ecr.us-east-2.amazonaws.com/dvc_viewer_backend_release
 export DVC_VIEWER_UI_IMAGE=260760892802.dkr.ecr.us-east-2.amazonaws.com/dvc_viewer_ui_release
-export DVC_VIEWER_RELEASE_VERSION=v0.5.1
+export DVC_VIEWER_RELEASE_VERSION=v0.7.0
 
 EXEC="docker-compose --no-ansi"
 MANIFESTS=(-f ./docker-compose/base.yaml)
 
 RELEASED_VERSIONS=(
-  v0.5.1
+  latest
+  v0.7.0
+  v0.6.1
+  v0.6.0
 )
+
+declare -a env_errors
 
 usage () {
   echo "Usage: $0 [OPTIONS]"
@@ -21,11 +26,39 @@ usage () {
   echo "  --release-version v0.5.1"
 }
 
+print_supported_envs () {
+  echo "Supported envs:"
+  echo "  GITHUB_CLIENT_ID*"
+  echo "  GITHUB_SECRET_KEY*"
+  echo
+  echo "  GITHUB_WEBHOOK_URL"
+  echo "  GITHUB_WEBHOOK_SECRET"
+  echo
+  echo "  POSTGRES_URL"
+  echo "  REDIS_URL"
+  echo
+  echo "* - required"
+}
+
+check_requirements() {
+  if [ -z "$GITHUB_CLIENT_ID" ]; then
+    env_errors+=("MUST provide GITHUB_CLIENT_ID env")
+  fi
+
+  if [ -z "$GITHUB_SECRET_KEY" ]; then
+    env_errors+=("MUST provide GITHUB_SECRET_KEY env")
+  fi
+}
+
 while [ $# -ne 0 ]; do
   case $1 in
     --ascii)
       EXEC="docker-compose"
       shift 1
+      ;;
+    --envs)
+      print_supported_envs
+      exit 0
       ;;
     --no-postgres)
       NO_POSTGRES=1
@@ -35,7 +68,7 @@ while [ $# -ne 0 ]; do
       NO_REDIS=1
       shift 1
       ;;
-    --help)
+    -h|--help)
       usage
       exit 0
       ;;
@@ -59,13 +92,28 @@ while [ $# -ne 0 ]; do
   esac
 done
 
+check_requirements
 
 if [ "$NO_POSTGRES" != "1" ]; then
   MANIFESTS+=(-f ./docker-compose/postgres.yaml)
+else
+  if [ -z "$POSTGRES_URL" ]; then
+    env_errors+=("MUST provide POSTGRES_URL env")
+  fi
 fi
 
 if [ "$NO_REDIS" != "1" ]; then
   MANIFESTS+=(-f ./docker-compose/redis.yaml)
+else
+  if [ -z "$REDIS_URL" ]; then
+    env_errors+=("MUST provide REDIS_URL env")
+  fi
+fi
+
+if [ ${#env_errors[@]} -ne 0 ]; then
+  ( IFS=$'\n'; echo "${env_errors[*]}" )
+  echo "more info: $0 --envs"
+  exit 1
 fi
 
 set -ex
