051aca1364d4931ee7fa82128bc657f38cdf60e3
Jesper Svendsen <99078145+jesper7@users.noreply.github.com>
2023-03-20 12:54:27 +0100

    Remove the Helm chart's default values.yaml in AMI (#38)

--- packer/setup_ubuntu.sh
+++ packer/setup_ubuntu.sh
@@ -6,9 +6,3 @@ set -o pipefail
 
 # Add Helm Iterative Repository
 helm repo add iterative https://helm.iterative.ai
-
-# Add Helm default values from latest release
-helm pull iterative/studio
-tar -zxvf studio-*.tgz -C /tmp
-mv /tmp/studio/values.yaml ~/studio-values.yaml
-rm studio-*.tgz
