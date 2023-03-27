8d3458355cdf89325a8f2692f0684cbc19cef8eb
Jesper Svendsen <jesper@iterative.ai>
2023-03-27 15:35:36 +0200

    Clean up repository, update README

--- terraform/modules/vpc/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "id" {
-  value = var.create_vpc ? module.vpc[0].vpc_id : data.aws_vpc.existing_vpc[0].id
-}
-
-output "private_subnet_ids" {
-  value = var.create_vpc ? module.vpc[0].private_subnets : data.aws_subnets.existing_vpc[0].ids
-}
