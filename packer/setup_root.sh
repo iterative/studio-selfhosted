78a1551f9707108c4986d73374a97e76eb3db1f2
Marcin Jasion <mjasion@users.noreply.github.com>
2023-04-12 13:46:42 +0200

    Change parameters exports and fix log script location
    `setup_root.sh` were failing on export-logs because there were no file in `/tmp`

--- packer/setup_root.sh
+++ packer/setup_root.sh
@@ -12,15 +12,15 @@ fi
 export DEBIAN_FRONTEND=noninteractive
 
 # Install K3s - script uploaded with packer
-K3S_VERSION="v1.25.7+k3s1"
-K3S_KUBECONFIG_MODE="644"
-INSTALL_K3S_VERSION="${K3S_VERSION}"
-INSTALL_K3S_SKIP_START="true"
-INSTALL_K3S_EXEC=""
-INSTALL_K3S_EXEC="$INSTALL_K3S_EXEC --disable=traefik"
-INSTALL_K3S_EXEC="$INSTALL_K3S_EXEC --kubelet-arg kube-reserved=cpu=500m,memory=1Gi,ephemeral-storage=1Gi"
-INSTALL_K3S_EXEC="$INSTALL_K3S_EXEC --kubelet-arg system-reserved=cpu=500m,memory=1Gi,ephemeral-storage=1Gi"
-INSTALL_K3S_EXEC="$INSTALL_K3S_EXEC --kubelet-arg eviction-hard=memory.available<0.5Gi,nodefs.available<10%"
+export K3S_VERSION="v1.25.7+k3s1"
+export K3S_KUBECONFIG_MODE="644"
+export INSTALL_K3S_VERSION="${K3S_VERSION}"
+export INSTALL_K3S_SKIP_START="true"
+export INSTALL_K3S_EXEC=""
+export INSTALL_K3S_EXEC="$INSTALL_K3S_EXEC --disable=traefik"
+export INSTALL_K3S_EXEC="$INSTALL_K3S_EXEC --kubelet-arg kube-reserved=cpu=500m,memory=1Gi,ephemeral-storage=1Gi"
+export INSTALL_K3S_EXEC="$INSTALL_K3S_EXEC --kubelet-arg system-reserved=cpu=500m,memory=1Gi,ephemeral-storage=1Gi"
+export INSTALL_K3S_EXEC="$INSTALL_K3S_EXEC --kubelet-arg eviction-hard=memory.available<0.5Gi,nodefs.available<10%"
 sh /home/ubuntu/.studio_install/k3s.sh -
 echo KUBECONFIG="/etc/rancher/k3s/k3s.yaml" >> /etc/environment
 
@@ -29,7 +29,7 @@ mkdir -p /var/lib/rancher/k3s/agent/images/
 curl "https://github.com/k3s-io/k3s/releases/download/${K3S_VERSION}/k3s-airgap-images-amd64.tar" -L -o /var/lib/rancher/k3s/agent/images/k3s-airgap-images-amd64.tar
 
 # Install k9s
-K9S_VERSION=v0.27.3
+export K9S_VERSION=v0.27.3
 cd /tmp
 curl --silent -L https://github.com/derailed/k9s/releases/download/${K9S_VERSION}/k9s_Linux_amd64.tar.gz -o /tmp/k9s_Linux_amd64.tar.gz
 echo "b0eb5fb0decedbee5b6bd415f72af8ce6135ffb8128f9709bc7adcd5cbfa690b  k9s_Linux_amd64.tar.gz" > /tmp/k9s.sha256
@@ -82,4 +82,5 @@ bash /home/ubuntu/.studio_install/helm3.sh
 helm repo add iterative https://helm.iterative.ai
 
 # Move the log exporter script
-mv /tmp/export-studio-logs /usr/local/bin/export-studio-logs
+cp /home/ubuntu/.studio_install/export-studio-logs /usr/local/bin/export-studio-logs
+chmod +x /usr/local/bin/export-studio-logs
