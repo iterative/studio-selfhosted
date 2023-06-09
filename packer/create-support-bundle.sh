ee9e67f5e2039a5d004710c3ad73f17214eabed1
Jesper Svendsen <99078145+jesper7@users.noreply.github.com>
2023-06-09 15:22:39 +0200

    Include DNS resolution info in support bundle (#67)

--- packer/create-support-bundle.sh
+++ packer/create-support-bundle.sh
@@ -31,15 +31,44 @@ done
 
 mkdir -p "$LOG_DIR"
 
-get_logs() {
-    POD_NAME=$(kubectl get pods --namespace $NAMESPACE -l "app.kubernetes.io/name=studio-$1,app.kubernetes.io/instance=studio" -o jsonpath="{.items[0].metadata.name}")
-    kubectl logs $POD_NAME --namespace $NAMESPACE
+k8s_cm_hostname_to_url() {
+  kubectl get configmap/studio --namespace $NAMESPACE -o=jsonpath="{.data.$1}" | sed -E 's#^(https?://)?([^:/?]+).*#\2#'
 }
 
+get_logs() {
+  POD_NAME=$(kubectl get pods --namespace $NAMESPACE -l "app.kubernetes.io/name=studio-$1,app.kubernetes.io/instance=studio" -o jsonpath="{.items[0].metadata.name}")
+  kubectl logs $POD_NAME --namespace $NAMESPACE
+}
+
+get_hostname() {
+  nslookup "$1"
+  dig "$1"
+}
+
+# Kubernetes
 get_logs backend > "$LOG_DIR/backend.txt"
 get_logs beat > "$LOG_DIR/beat.txt"
 get_logs ui > "$LOG_DIR/ui.txt"
 get_logs worker > "$LOG_DIR/worker.txt"
+
+# DNS resolution
+STUDIO_HOSTNAME=$(k8s_cm_hostname_to_url "UI_URL")
+get_hostname "$STUDIO_HOSTNAME" > "$LOG_DIR/dns_studio.txt"
+
+GITLAB_HOSTNAME=$(k8s_cm_hostname_to_url "GITLAB_URL")
+if [ -n "$GITLAB_HOSTNAME" ]; then
+  get_hostname "$GITLAB_HOSTNAME" > "$LOG_DIR/dns_gitlab.txt"
+fi
+
+GITHUB_HOSTNAME=$(k8s_cm_hostname_to_url "GITHUB_URL")
+if [ -n "$GITHUB_HOSTNAME" ]; then
+  get_hostname "$GITHUB_HOSTNAME" > "$LOG_DIR/dns_github.txt"
+fi
+
+BITBUCKET_HOSTNAME=$(k8s_cm_hostname_to_url "BITBUCKET_URL")
+if [ -n "$BITBUCKET_HOSTNAME" ]; then
+  get_hostname "$BITBUCKET_HOSTNAME" > "$LOG_DIR/dns_bitbucket.txt"
+fi
 
 tar -zcvf /tmp/studio-support.tar.gz "$LOG_DIR"
 rm -rf "$LOG_DIR"
